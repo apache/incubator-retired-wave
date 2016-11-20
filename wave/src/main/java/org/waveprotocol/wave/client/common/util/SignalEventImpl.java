25c7556332f3ca0adef43487016fdf6767891c58
wisebaldone <wisebaldone@apache.org>
2016-11-20 15:29:44 +1000

    Hot fix for editor misbehaving in chrome

--- wave/src/main/java/org/waveprotocol/wave/client/common/util/SignalEventImpl.java
+++ wave/src/main/java/org/waveprotocol/wave/client/common/util/SignalEventImpl.java
@@ -90,9 +90,10 @@ public class SignalEventImpl implements SignalEvent {
     return KEY_EVENTS.contains(event.getType());
   }
 
-  private static final UserAgentType currentUserAgent =
-      (UserAgent.isWebkit() ? UserAgentType.WEBKIT : (
-          UserAgent.isFirefox() ? UserAgentType.GECKO : UserAgentType.IE));
+  //TODO: check requirements if seperate logic is still needed.
+  private static final UserAgentType currentUserAgent = UserAgentType.GECKO;
+  //    (UserAgent.isWebkit() ? UserAgentType.WEBKIT : (
+  //        UserAgent.isFirefox() ? UserAgentType.GECKO : UserAgentType.IE));
   private static final OperatingSystem currentOs =
       (UserAgent.isWin() ? OperatingSystem.WINDOWS : (
           UserAgent.isMac() ? OperatingSystem.MAC : OperatingSystem.LINUX));
